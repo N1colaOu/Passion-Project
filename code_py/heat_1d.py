51e41b89d1c3449d3d3df0c02370e9600c1598be
N1colaOu <n.ouzounov06@gmail.com>
2026-04-01 19:02:34 +0200

    Minor heat python fix

--- code_py/heat_1d.py
+++ code_py/heat_1d.py
@@ -11,7 +11,7 @@ x_max = 100 #cm
 t_max = 30 #s
 
 u_max = 2 #max_temp for random gen
-u_min = -2 #min_temp for random gen
+u_min = -2 #miNemp for random gen
 
 u_start = 0   #boundary condition
 u_end = 0   #boundary condition
@@ -25,38 +25,32 @@ def uxx(u, x, t):
 
 
 T0 = np.random.randint(u_min, u_max, N)
-n_t = N*2
-n_x = N
-u = np.zeros((n_x, n_t))
+u = np.zeros((N, N))
 T0[0] = u_start
 T0[-1] = u_end
 u[:, 0] = T0[:]
-dt = t_max/n_t #step size in t
-dx = x_max/n_x #step size in x
+dt = t_max/N #step size in t
+dx = x_max/N #step size in x
 #du_dxx = np.zeros_like(u)
 du_dxx = np.copy(u)
 #du_dt = np.zeros_like(u)
 du_dt = np.copy(u)
 
 coeff = dt*coeff/(dx**2)
-for j in range(n_t-1):
-    for i in range(1,n_x-1):
+for j in range(N-1):
+    for i in range(1,N-1):
         du_dxx[i, j] = uxx(u, i, j) #get curr_xx value for all points on x
     du_dt[:, j] = du_dxx[:, j]*coeff #get curr_t value for all points on x
-    for i in range(n_x):
+    for i in range(N):
         u[i, j+1] = u[i, j] + du_dt[i, j] #get temp value for all points on x after dt has passed
 
-t = np.linspace(0, t_max, n_t) 
-x = np.linspace(0, x_max, n_x)
-#x, t = np.meshgrid(x, t)
+t = np.linspace(0, t_max, N) 
+x = np.linspace(0, x_max, N)
 
-"""
+x, t = np.meshgrid(x, t)
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize = (10, 9))
-ax.plot_surface(t, x, u, edgecolor='none', cmap='viridis')
-ax.set_title('One Dimensional Numerical Heat Equation')
-ax.set_ylabel('Position on rod')
-ax.set_xlabel('Position in time')
-ax.set_zlabel('Heat')
+ax.plot_surface(x, t, u, edgecolor='none', cmap='viridis')
+ax.set(xlim=[0, x_max], ylim = [0, t_max] , zlim=[u_min, u_max], ylabel='Positon [m]', xlabel='Time [s]', zlabel = "Temp [C]", title = "One Dimensional Numerical Heat Equation")
 plt.show()
 """
 fig, ax = plt.subplots(figsize = (10, 9))
@@ -67,8 +61,9 @@ def update(frame):
     ydata = u[:, frame]
     line.set_data(x, ydata)
     return line,
-ani = animation.FuncAnimation(fig=fig, func=update, frames=n_t, interval=30)
+ani = animation.FuncAnimation(fig=fig, func=update, frames=N, interval=30)
 plt.show()
+"""
 #ani.save("heat_1d.gif")
 
 
